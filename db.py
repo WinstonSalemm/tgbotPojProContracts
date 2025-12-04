9e51829f2675780072c04d488b31c768303d8a04
Tima <winston234123@gmail.com>
2025-12-04 17:40:34 +0500

    2 update

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 import psycopg2
 import os
-import json
 
 def db():
     return psycopg2.connect(
@@ -15,44 +14,25 @@ def init_tables():
     conn = db()
     cur = conn.cursor()
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS contracts (
-            id SERIAL PRIMARY KEY,
-            buyer_name TEXT,
-            inn TEXT,
-            address TEXT,
-            phone TEXT,
-            account TEXT,
-            bank TEXT,
-            mfo TEXT,
-            director TEXT,
-            items JSONB,
-            file_url TEXT,
-            created_at TIMESTAMP DEFAULT NOW()
-        );
+    CREATE TABLE IF NOT EXISTS contracts (
+        id SERIAL PRIMARY KEY,
+        buyer_name TEXT,
+        inn TEXT,
+        phone TEXT,
+        total_sum NUMERIC,
+        file_url TEXT,
+        created_at TIMESTAMP DEFAULT NOW()
+    );
     """)
     conn.commit()
     conn.close()
 
-def save_contract(data: dict, file_url: str):
+def save_contract(name, inn, phone, total, url):
     conn = db()
     cur = conn.cursor()
-
     cur.execute("""
-        INSERT INTO contracts
-        (buyer_name, inn, address, phone, account, bank, mfo, director, items, file_url)
-        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-    """, (
-        data["buyer_name"],
-        data["inn"],
-        data["address"],
-        data["phone"],
-        data["account"],
-        data["bank"],
-        data["mfo"],
-        data["director"],
-        json.dumps(data["items"]),
-        file_url
-    ))
-
+        INSERT INTO contracts(buyer_name, inn, phone, total_sum, file_url)
+        VALUES (%s, %s, %s, %s, %s)
+    """,(name, inn, phone, total, url))
     conn.commit()
     conn.close()
