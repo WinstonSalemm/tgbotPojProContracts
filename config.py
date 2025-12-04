d9f3b99f44dffdc7a66076fa9b49b643db97b7d2
Tima <winston234123@gmail.com>
2025-12-04 16:59:53 +0500

    1 bag

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_TOKEN = os.getenv("7245964747:AAGbYsL_3--NZRh-rhTWsLX7aLELIvbZvjw")
+API_TOKEN = os.getenv("BOT_TOKEN")
 API_ENDPOINT = os.getenv(
     "API_ENDPOINT",
     "http://localhost:5016/agreements/generate"  # дефолт, пока API локально
