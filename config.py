e75362e4ceb281d6148bcda53348a54be10dba86
Tima <winston234123@gmail.com>
2025-12-05 15:04:42 +0500

    2 5.12

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 API_TOKEN = os.getenv("BOT_TOKEN")
 API_ENDPOINT = os.getenv(
     "API_ENDPOINT",
-    "http://localhost:5016/agreements/generate"  # дефолт, пока API локально
+    "https://apipojprocontracts-production.up.railway.app/"
 )
